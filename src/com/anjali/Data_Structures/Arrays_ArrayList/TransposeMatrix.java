3ff84318ecbf6d2e6411fdf5a1a23e7b55ebfffa
anjaliarakka <anjaliarakka07@gmail.com>
2026-03-07 17:36:14 +0530

    Leet code submissions

--- src/com/anjali/Data_Structures/Arrays_ArrayList/TransposeMatrix.java
+++ src/com/anjali/Data_Structures/Arrays_ArrayList/TransposeMatrix.java
@@ -42,6 +42,18 @@ public class TransposeMatrix {
 //            } // works with arraylist
 //        }
 
+//        public int[][] transpose(int[][] matrix) {
+//            int[][] ans = new int[matrix[0].length][matrix.length];
+//
+//            for (int i = 0; i < matrix.length; i++) {
+//                for (int j = 0; j < matrix[0].length; j++) {
+//                    int temp = matrix[i][j];
+//                    ans[j][i] = matrix[i][j];
+//                    matrix[i][j] = temp;           }
+//            }
+//            return ans;
+//        }
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                  if (i != j) {

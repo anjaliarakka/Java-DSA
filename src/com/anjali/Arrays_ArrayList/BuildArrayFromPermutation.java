ad4f2c6845ae20d4d7c0901cd67de4e01c169737
anjaliarakka <anjaliarakka07@gmail.com>
2026-03-07 17:36:13 +0530

    Example questions

---
+++ src/com/anjali/Arrays_ArrayList/BuildArrayFromPermutation.java
@@ -0,0 +1,24 @@
+package com.anjali.Arrays_ArrayList;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BuildArrayFromPermutation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] nums = new int[6];
+
+        System.out.print("Enter 6 numbers: ");
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        int[] ans = new int[6];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        System.out.print(Arrays.toString(ans));
+    }
+}
